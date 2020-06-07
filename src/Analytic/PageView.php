342109b5c003a0bf63bac2c79db2e982bd399393
George Torres <george@datonique.com>
2020-06-07 15:50:26 -0700

    started the oath2, and added required fields for session and the two events

--- src/Analytic/PageView.php
+++ src/Analytic/PageView.php
@@ -5,23 +5,31 @@ use datonique\Analytic\Analytic;
 
 class PageView extends Analytic {
     
-    private $page_name;
-
+    private $html_page_title;
+    private $page_php_class_name;
+    private $page_url;
     /**
      * Constructor
      *
      * @param string                $button_name
      */
-    public function __construct(string $page_name)
+    public function __construct(
+        string $html_page_title, 
+        string $page_php_class_name, 
+        string $page_url)
     {
-        parent::__construct('PAGE VIEW');
-        $this->page_name = $page_name;
+        parent::__construct('pageview');
+        $this->html_page_title = $html_page_title;
+        $this->page_php_class_name = $page_php_class_name;
+        $this->page_url = $page_url;
     }
 
     public function toOutArray() 
     {
         return array_merge(parent::toOutArray(), array(
-            'page_name' => $this->page_name
+            'html_page_title' => $this->html_page_title,
+            'page_php_class_name' => $this->page_php_class_name,
+            'page_url' => $this->page_url,
         ));
     }
 }
--- tests/Analytic/AnalyticTest.php
+++ tests/Analytic/AnalyticTest.php
@@ -24,23 +24,36 @@ class AnalyticTest extends TestCase
 
     public function testPageView()
     {
-        $page_view = new PageView('test_page');
+        $page_view = new PageView(
+            'test_page_html',
+            'test_page_php_class_name',
+            'test_page_url'
+        );
         $page_view->setSession(new Session());
         $out_array = $page_view->toOutArray();
         $this->assertTrue(SessionTestHelper::testSessionInfoForAnalytic($out_array));
-        $this->assertEquals($out_array['event_name'], 'PAGE VIEW');
-        $this->assertEquals($out_array['page_name'], 'test_page');
+        $this->assertEquals($out_array['event_name'], 'pageview');
+        $this->assertEquals($out_array['html_page_title'], 'test_page_html');
+        $this->assertEquals($out_array['page_php_class_name'], 'test_page_php_class_name');
+        $this->assertEquals($out_array['page_url'], 'test_page_url');
     }
 
     public function testButtonClick()
     {
-        $page_view = new ButtonClick('test_button', 'test_page');
+        $page_view = new ButtonClick(
+            'test_button', 
+            'test_page_html',
+            'test_page_php_class_name',
+            'test_page_url'
+        );
         $page_view->setSession(new Session());
         $out_array = $page_view->toOutArray();
         $this->assertTrue(SessionTestHelper::testSessionInfoForAnalytic($out_array));
-        $this->assertEquals($out_array['event_name'], 'BUTTON CLICK');
+        $this->assertEquals($out_array['event_name'], 'button_click');
         $this->assertEquals($out_array['button_name'], 'test_button');
-        $this->assertEquals($out_array['page_name'], 'test_page');
+        $this->assertEquals($out_array['html_page_title'], 'test_page_html');
+        $this->assertEquals($out_array['page_php_class_name'], 'test_page_php_class_name');
+        $this->assertEquals($out_array['page_url'], 'test_page_url');
     }
 }
 
@@ -48,7 +61,7 @@ class SessionTestHelper
 {
     public static function testSessionInfoForAnalytic($out_array) 
     {
-        return (isset($out_array['timestamp']) && 
+        return (isset($out_array['unix_timestamp']) && 
             isset($out_array['session_id']));
     }
 }
