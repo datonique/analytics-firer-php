11089bca3227ff1b9b41205e552e138ea053f463
George Torres <george@datonique.com>
2020-06-13 11:57:36 -0700

    OS, OS version, Browser, Browser version, mobile or web

--- src/Session/Session.php
+++ src/Session/Session.php
@@ -8,98 +8,13 @@ class Session {
     /**
      * Required
      */
-    private $session_id;
-    private $os;
-    private $os_version;
-    private $browswer;
-    private $broweser_version;
-    private $web_or_mobile;
-    private $product_description;
-    private $product_shortname;
-
-    /**
-     * Optional User Information
-     */
-    private $visitor_id;
-    private $user_id;
-    private $uas_user_id;
-    private $user_first_name;
-    private $user_last_name;
-    private $user_email;
-    private $profession_id;
-    private $profession_title;
-
-    static $COOKIE_SESSION_ID = 'datonique_session_id';
-    /**
-     * Constructor
-     *
-     * @param string                $button_name
-     */
-    public function __construct(Cookie $cookie)
-    {
-        $this->cookie = $cookie;
-        $this->session_id = $cookie->getCookie(Session::$COOKIE_SESSION_ID);
-        if (is_null($this->session_id)) {
-            $this->session_id = $this->getGuidV4();
-            $this->cookie->setCookie(Session::$COOKIE_SESSION_ID, $this->session_id);
-        }
-    }
-
-    public function toOutArray()
-    {
-        return array(
-            'session_id' => $this->session_id,
-            'os' => $this->os,
-            'os_version' => $this->os_version,
-            'browswer' => $this->browswer,
-            'broweser_version' => $this->broweser_version,
-            'web_or_mobile' => $this->web_or_mobile,
-            'product_description' => $this->product_description,
-            'product_shortname' => $this->product_shortname,
-            'visitor_id' => $this->session_id,
-            'user_id' => $this->user_id,
-            'uas_user_id' => $this->uas_user_id,
-            'user_first_name' => $this->user_first_name,
-            'user_last_name' => $this->user_last_name,
-            'user_email' => $this->user_email,
-            'profession_id' => $this->profession_id,
-            'profession_title' => $this->profession_title,
-        );
-    }
-
-    private function isVisitor() 
-    {
-        return is_null($this->user_id);
-    }
-
-    // TODO: set userID
-    private function getGuidV4()
-    {
-        $data = openssl_random_pseudo_bytes(16);
-        assert(strlen($data) == 16);
-
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-    }
-}
-<?php
-
-namespace datonique\Session;
-
-class Session {
-    private $cookie;
-
-    /**
-     * Required
-     */
     public $session_id;
     private $os;
     private $os_version;
     private $browser;
     private $broweser_version;
     private $web_or_mobile;
+    
     private $product_description;
     private $product_shortname;
 
@@ -116,7 +31,6 @@ class Session {
     private $profession_title;
 
     static $COOKIE_SESSION_ID = 'datonique_session_id';
-    private $user_aget;
     /**
      * Constructor
      *
@@ -130,9 +44,20 @@ class Session {
             $this->session_id = $this->getGuidV4();
             $this->cookie->setCookie(Session::$COOKIE_SESSION_ID, $this->session_id);
         }
-        $this->user_agent = $_SERVER['HTTP_USER_AGENT'];
-        $this->os = $this->getOS();
-        $this->browser = $this->getBrowser();
+        $browser = new Browser();
+        $client_info = $browser->browser_detection('full_assoc');
+        $this->os = $client_info['os'];
+        $this->os_version = $client_info['os_number'];
+        $this->browser = $client_info['browser_name'];
+        $this->browser_version = $client_info['browser_number'];
+        
+        if ($client_info['ua_type'] == 'mobile') {
+            $this->web_or_mobile = 'mobile';
+        } else if ($client_info['ua_type'] == 'bro' || $client_info['ua_type'] == 'bbro' ) {
+            $this->web_or_mobile = 'web';
+        } else {
+            $this->web_or_mobile = 'other';
+        }
     }
 
     public function toOutArray()
@@ -142,7 +67,7 @@ class Session {
             'os' => $this->os,
             'os_version' => $this->os_version,
             'browser' => $this->browser,
-            'broweser_version' => $this->broweser_version,
+            'broweser_version' => $this->browser_version,
             'web_or_mobile' => $this->web_or_mobile,
             'product_description' => $this->product_description,
             'product_shortname' => $this->product_shortname,
@@ -154,7 +79,6 @@ class Session {
             'user_email' => $this->user_email,
             'profession_id' => $this->profession_id,
             'profession_title' => $this->profession_title,
-            'user_aget' => $this->user_agent,
         );
     }
 
@@ -209,21 +133,15 @@ class Session {
                             );
 
         foreach ($os_array as $regex => $value) { 
-
             if (preg_match($regex, $this->user_agent)) {
                 $os_platform    =   $value;
             }
-
         }   
 
         return $os_platform;
-
     }
 
     private function getBrowser() {
-
-        // global $user_agent;
-        
         if (is_null($this->user_agent)) {
             throw new Exception("no user agent to get browser");
         }
@@ -242,15 +160,11 @@ class Session {
                             );
 
         foreach ($browser_array as $regex => $value) { 
-
             if (preg_match($regex, $this->user_agent)) {
                 $browser    =   $value;
             }
-
         }
 
         return $browser;
-
     }
-
 }
